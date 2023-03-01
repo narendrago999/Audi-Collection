cc70f4d05a669f4a4d1e3b6eaa7dcacb1b9eee9d
Narendra Godara <95878375+narendrago999@users.noreply.github.com>
2023-03-01 20:37:05 +0530

    Update settings.py

--- carSell/settings.py
+++ carSell/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-oq0nmg7%(a%!y_=sx4q8ym^ibxef$117t00bdakknh!mnk*@y9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','']
 
 
 # Application definition
